ee9f32b7cf362ae6a8e192101da2c73829ceec14
Nahida Zaman Bina <nahidazmn@gmail.com>
2025-03-17 14:44:49 +0600

    button url added

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -3,7 +3,8 @@
 const Navbar = () => (
   <nav className="bg-[#003580] text-white py-2.5">
     <div className="container max-w-[1200px] mx-auto px-4 flex justify-between items-center">
-      <h1 className="text-xl sm:text-2xl font-bold">Booking.com</h1>
+      <h1 className="text-xl sm:text-2xl font-bold">
+        <a href="/">Booking.com</a></h1>
       <div className="flex items-center gap-2 sm:gap-5">
         {/* Currency Button */}
         <button className="bg-white text-[#003580] px-2 py-1 sm:px-4 sm:py-2 rounded-md text-xs sm:text-sm">
